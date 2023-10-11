e66255b68e0064b53583a5ab2dd9be5bef2970ca
hiteshjain48 <hiteshjainhd@gmail.com>
2023-10-12 02:08:32 +0530

    initial commit with css and file setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloneLin } from 'react-icons/ri'
+const Navbar = () => {
+  return (
+    <div className='gtp3__navbar'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
